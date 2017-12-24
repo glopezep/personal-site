819fddeffc9f3e40b31cd330ce7857e5c74e6332
Guillermo Lopez <guillermolopez2529@gmail.com>
2017-12-24 12:01:30 -0400

    meta vp added

--- src/shared/components/Layout.js
+++ src/shared/components/Layout.js
@@ -6,6 +6,10 @@ function Layout (props) {
   return (
     <section className='main-container'>
       <Head>
+        <meta 
+          name="viewport" 
+          content="width=device-width, initial-scale=1.0"
+        />
         <link 
           rel="stylesheet" 
           href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" 
