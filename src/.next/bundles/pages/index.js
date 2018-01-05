c3dc670a0204c146dca2c5df670e57b067737ecb
Guillermo Lopez <guillermolopez2529@gmail.com>
2018-01-05 00:09:52 -0400

    contact added

--- src/.next/bundles/pages/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([2],[],[421]);
-            return { page: comp.default }
-          })
-        
